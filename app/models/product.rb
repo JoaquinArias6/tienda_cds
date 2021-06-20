ae7aefbc50f573a39fd165671bb1fcd4f3b966ea
JoaquinArias6 <84819635+JoaquinArias6@users.noreply.github.com>
2021-06-19 23:53:48 -0300

    Added image support to products

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,8 @@ class Product < ApplicationRecord
 	validates :name, presence: true, uniqueness: true
 	validates :description, presence: true
 	validates :price, presence: true, numericality: { greater_than: 0 }
+
+	validates :image_url, allow_blank: true, format: {
+        with: %r{\.(gif|jpg|png)\z}i
+    }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_15_004419) do
+ActiveRecord::Schema.define(version: 2021_06_20_014758) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2021_06_15_004419) do
     t.integer "price"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image_url"
   end
 
 end
